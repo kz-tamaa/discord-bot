37474774f9bde9b65b28975a016bee20a8889c67
Kazumasa Tamtsu <k.t@k-tamatsu.local>
2018-12-23 23:01:01 +0900

    music配下のmp3ファイルの表示、再生、停止実装

--- music_bot.py
+++ music_bot.py
@@ -1,12 +1,17 @@
 import discord
 from discord.ext import commands
-from discord.voice_client import VoiceClient
-import asyncio
+from glob import glob
+from pathlib import Path
+import os
 
 
-bot = commands.Bot(command_prefix="|")
+bot = commands.Bot(command_prefix='!')
 client = discord.Client()
-vc = None
+voice = None
+player = None
+
+MUSIC_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/music'
+
 
 # 起動時に通知してくれる処理
 @client.event
@@ -17,19 +22,56 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    channel = client.get_channel(message.author.voice_channel.id)
-    if message.content.startswith('/bye'):
-        if client.is_voice_connected(channel.server):
-            voice = client.voice_client_in(channel.server)
-        else:
-            voice = await client.join_voice_channel(channel)
-        player = voice.create_ffmpeg_player('voicetext_02.mp3')
-        player.start()
-    if message.content.startswith('/inu'):
-        vc = await client.join_voice_channel(channel)
-        print(vc)
-        reply = 'わん'
-        await client.send_message(message.channel, reply)
+    global voice, player
+    if message.author.bot:
+        # 発言者がBotの場合
+        return
+    if message.content.startswith('!list'):
+        # ローカルに存在する音楽ファイルの一覧を表示
+        music_list = glob(MUSIC_FOLDER + '/*.mp3')
+        embed = discord.Embed(title="Music List", description="以下、音楽リストです。", color=0xeee657)
+        for i, music in enumerate(music_list):
+            embed.add_field(name=str(i) + '\t:\t' + Path(music).stem, value=Path(music).stem, inline=False)
+        await client.send_message(message.channel, embed=embed)
+    if message.content.startswith('!play') and len(message.content.split()) == 2:
+        if is_not_joined_voice_channel(message):
+            await client.send_message(message.channel, 'ボイスチャンネルに参加した上で実行してください！！')
+            return
+        if voice is None:
+            voice = await client.join_voice_channel(message.author.voice_channel)
+        elif voice.is_connected():
+            if player is not None and player.is_playing():
+                player.stop()
+        music_list = glob(MUSIC_FOLDER + '/*.mp3')
+        try:
+            music_path = music_list[int(message.content.split()[1])]
+            player = voice.create_ffmpeg_player(music_path)
+            player.start()
+            embed = discord.Embed(title='Now Playing!', description=Path(music_path).stem)
+            await client.send_message(message.channel, embed=embed)
+        except IndexError:
+            await client.send_message(message.channel, '曲が存在しません！')
+    if message.content.startswith('!stop') and player is not None and player.is_playing():
+        if is_not_joined_voice_channel(message):
+            await client.send_message(message.channel, 'ボイスチャンネルに参加した上で実行してください！！')
+            return
+        player.stop()
+
+
+def is_not_joined_voice_channel(message) -> bool:
+    return message.author.voice_channel is None
+
+
+# @bot.command()
+# async def add():
+#     print('aa')
+#
+#
+# @bot.event()
+# async def on_message(message):
+#     await bot.process_commands(message)
+#     await client.send_message(message.channel, 'うぇい')
 
 
 client.run('NTI0NTU5ODQ2MjQ3MjM1NTk0.Dvp2Dg.zNPhkoF3AfVkvmmWuh1AuT4zJu8')
+# bot.run('NTI0NTU5ODQ2MjQ3MjM1NTk0.Dvp2Dg.zNPhkoF3AfVkvmmWuh1AuT4zJu8')
